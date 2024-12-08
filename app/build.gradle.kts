25654994fcb3b7147725239323c33a99ac9631e5
Aleale012 <sayafarhangustimungkas@gmail.com>
2024-12-08 22:12:31 +0700

    Add database&google.services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation ("com.google.firebase:firebase-auth-ktx:23.1.0")
+    implementation (libs.firebase.firestore.v2400)
 }
--- app/src/main/java/com/example/heartguardlayout/RegisterActivity.kt
+++ app/src/main/java/com/example/heartguardlayout/RegisterActivity.kt
@@ -3,27 +3,56 @@ package com.example.heartguardlayout
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        mAuth = FirebaseAuth.getInstance()
+
         val btnRegister = findViewById<Button>(R.id.btnRegister)
+        val emailEditText = findViewById<EditText>(R.id.etEmail)
+        val passEditText = findViewById<EditText>(R.id.etPassword)
 
-        // Pindah ke halaman Register
         btnRegister.setOnClickListener {
-            // Simulasi registrasi selesai
-            Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+            val email = emailEditText.text.toString().trim()
+            val password = passEditText.text.toString().trim()
 
-            // Pindah ke halaman Login setelah registrasi berhasil
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
-            // Menutup RegisterActivity setelah berpindah ke LoginActivity
-            finish()
+            authRegis(email, password)
         }
+    }
+
+    fun authRegis(email: String, pass: String) {
+        mAuth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+
+
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Registration Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                // Menangani kegagalan jika terjadi error
+                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
